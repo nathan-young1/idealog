be0364c3cd8070ab10c811d77b2921677e0f10f6
nathan-young1 <okoriejonathan123@gmail.com>
2021-06-07 04:45:25 +0100

    removed sync now from background

--- android/app/src/main/java/com/mobile/idealog/MainActivity.java
+++ android/app/src/main/java/com/mobile/idealog/MainActivity.java
@@ -1,37 +1,24 @@
 package com.mobile.idealog;
 
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
 import androidx.work.BackoffPolicy;
 import androidx.work.Constraints;
-import androidx.work.ExistingWorkPolicy;
 import androidx.work.NetworkType;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.Operation;
 import androidx.work.PeriodicWorkRequest;
-import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
 import dataSyncronization.AutoSync;
-import dataSyncronization.SyncNow;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends FlutterFragmentActivity {
     final private String AutoSyncWorkRequestTag = "AutoSync";
-    final private String SyncNowWorkRequestTag = "SyncNow";
     final private String startAutoSyncMethod = "startAutoSync";
     final private String cancelAutoSyncMethod = "cancelAutoSync";
-    final private String syncNowMethod = "syncNow";
     private static final String CHANNEL = "com.idealog.alarmServiceCaller";
 
     @Override
@@ -46,8 +33,6 @@ public class MainActivity extends FlutterFragmentActivity {
                 }else if(call.method.equals(cancelAutoSyncMethod)){
                     cancelAutoSync();
                     result.success("Auto Sync has been canceled");
-                }else  if(call.method.equals(syncNowMethod)){
-                    syncNow(result);
                 }
             }
         });
@@ -83,36 +68,5 @@ public class MainActivity extends FlutterFragmentActivity {
     public void cancelAutoSync(){
 //        cancel the autoSync work request by the tag
         WorkManager.getInstance(this).cancelAllWorkByTag(AutoSyncWorkRequestTag);
-    }
-
-
-    public void syncNow(MethodChannel.Result result){
-        Constraints workRequestConstraints = new Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build();
-
-
-        OneTimeWorkRequest syncNowWorkRequest = new OneTimeWorkRequest.Builder(SyncNow.class)
-                .setConstraints(workRequestConstraints)
-                .addTag(SyncNowWorkRequestTag)
-                .build();
-
-
-        WorkManager workManagerInstance = WorkManager.getInstance(this);
-
-        workManagerInstance.enqueueUniqueWork(SyncNowWorkRequestTag, ExistingWorkPolicy.APPEND_OR_REPLACE,syncNowWorkRequest);
-
-        workManagerInstance
-                .getWorkInfoByIdLiveData(syncNowWorkRequest.getId())
-                .observe(this, workInfo -> {
-                    switch (workInfo.getState()){
-                        case SUCCEEDED:
-                            result.success("Success");
-                            break;
-                        case FAILED:
-                            result.error("Failed","Failed",null);
-                            break;
-                    }
-                });
     }
 }
