731595f5abb5408c45bcc557eb43bbc2b5f8a30c
nathan-young1 <okoriejonathan123@gmail.com>
2021-07-28 11:12:32 +0100

    i added the completed page

--- lib/Idea/ui/DetailPage/views/Tasks/TaskList.dart
+++ lib/Idea/ui/DetailPage/views/Tasks/TaskList.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:idealog/Idea/ui/Others/AddTasks.dart';
-import 'package:idealog/Idea/ui/TaskManager/highPriority.dart';
+import 'package:idealog/Idea/ui/TaskManager/completedTasksPage.dart';
+import 'package:idealog/Idea/ui/TaskManager/highPriorityPage.dart';
 import 'package:idealog/Idea/ui/TaskManager/uncompletedTasksPage.dart';
 import 'package:idealog/SearchBar/SearchNotifier.dart';
 import 'package:idealog/core-models/ideaModel.dart';
@@ -118,7 +119,7 @@ class TaskManager extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             UncompletedTasksWidget(borderRadius: borderRadius, idea: idea),
-            CompletedTasksWidget(borderRadius: borderRadius)
+            CompletedTasksWidget(borderRadius: borderRadius, idea: idea)
           ],
         )
       ]
@@ -130,33 +131,41 @@ class TaskManager extends StatelessWidget {
 class CompletedTasksWidget extends StatelessWidget {
   const CompletedTasksWidget({
     Key? key,
+    required this.idea,
     required this.borderRadius,
   }) : super(key: key);
 
+  final Idea idea;
   final BorderRadius borderRadius;
 
   @override
   Widget build(BuildContext context) {
-    return Container(height: 130, width: 130,
-    decoration: BoxDecoration(borderRadius: borderRadius,color: completedTasksColor),
-    child: Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        Container(
-          height: 30,
-          width: 30,
-          decoration: BoxDecoration(shape: BoxShape.circle, color: Black242424.withOpacity(0.2)),
-          child: Icon(Icons.warning_amber_outlined,color: Colors.white,size: 18)
-        ),
-        Flexible(child: Text('Completed Tasks',style: overpass.copyWith(fontSize: 16, color: Colors.white))),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [Text('20/20',style: overpass.copyWith(fontSize: 14,color: Colors.white),)],)
-      ],),
-    ),);
+    return GestureDetector(
+      onTap: ()=>
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context)=> CompletedTasksPage(idea: idea))
+      ),
+      child: Container(height: 130, width: 130,
+      decoration: BoxDecoration(borderRadius: borderRadius,color: completedTasksColor),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Black242424.withOpacity(0.2)),
+            child: Icon(Icons.warning_amber_outlined,color: Colors.white,size: 18)
+          ),
+          Flexible(child: Text('Completed Tasks',style: overpass.copyWith(fontSize: 16, color: Colors.white))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [Text('20/20',style: overpass.copyWith(fontSize: 14,color: Colors.white),)],)
+        ],),
+      ),),
+    );
   }
 }
 
